c6f15e8e6071da43ee6c8222b00e5f20c3f54555
LidorPrototype <ivanlidor98@gmail.com>
2025-04-13 19:02:39 +0300

    Utilities file

---
+++ backend/utils/utils.py
@@ -0,0 +1,17 @@
+from pdfminer.high_level import extract_text
+
+from backend.parsers.schema import CandidateProfile
+
+def build_weighted_candidate_blob(candidate: CandidateProfile) -> str:
+    """
+    Builds a weighted string for TF-IDF where skills, titles, and education
+    contribute at different levels.
+    """
+    skill_blob = " ".join(candidate.skills) * 3
+    title_blob = " ".join(candidate.job_titles) * 2
+    edu_blob = " ".join(candidate.education)
+
+    return " ".join([skill_blob, title_blob, edu_blob])
+
+def extract_text_from_pdf(pdf_path):
+    return extract_text(pdf_path)
